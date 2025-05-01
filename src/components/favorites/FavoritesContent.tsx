88b4978d89e759f6a322aded11300a7b559f891c
Pana <panarandri@gmail.com>
2025-05-01 14:29:54 +0300

    fix: favorites content

---
+++ src/components/favorites/FavoritesContent.tsx
@@ -0,0 +1,31 @@
+import { useFavoriteStore } from "@/store/useFavoriteStore";
+import { useSearchStore } from "@/store/useSearchStore";
+import React from "react";
+import PokemonCard from "../commons/PokemonCard";
+
+const FavoritesContent = () => {
+  const { search } = useSearchStore();
+  const { favorites } = useFavoriteStore();
+
+  const filteredData = favorites?.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+  return (
+    <div className="flex space-x-2">
+      {favorites && (
+        <div
+          className={`
+            grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 
+            grid-rows-1 sm:grid-row-2 md:grid-row-2 2xl:grid-row-3 gap-x-8 gap-y-16 w-full
+          `}
+        >
+          {filteredData?.map((pokemon, index: number) => (
+            <PokemonCard key={index} pokemon={pokemon} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FavoritesContent;
